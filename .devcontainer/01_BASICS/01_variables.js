f8e3ade1cff3b28e6e58635e7f24271d2a02bf81
arpan-bhati-developer <147476575+arpan-bhati-developer@users.noreply.github.com>
2023-10-10 09:30:55 +0000

    changes done

---
+++ .devcontainer/01_BASICS/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1445;
+let accountEmail = "arpan@google.com";
+var accountPassword = "1234";
+accountCity = "Meerut";
+
+// console.log(accountId);
+
+accountEmail = "harsh@google.com";
+accountPassword = "2323";
+accountCity = "Jaipur";
+console.table([accountEmail,accountPassword,accountCity]);
