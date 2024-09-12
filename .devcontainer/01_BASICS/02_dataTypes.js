a3e7f1e5a69965eb2c8de9889c1a16b9ac5ac663
arpan-bhati-developer <147476575+arpan-bhati-developer@users.noreply.github.com>
2024-09-12 11:40:20 +0000

    Datatypes

---
+++ .devcontainer/01_BASICS/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node js ,not browser so we can't run alert here 
+
+console.log(3 
+    + 3)// code readability should be high, it should be written as console.log(3 + 3)
+
+console.log("Arpan")
+
+let name = "Arpan";
+let age = 24;
+let isLoggedIn = false;
+
+//number => range is 2 to power 53.
+// bigInt => used to store a vert big number.
+// String => "" always give value in double quotes ("").
+// boolean => stores true/false.
+// null => standalone value.
+// undefined => when we declared variable and don't provided any value then it will contain valur as undefined.
+// symbol => to find uniqueness among the components.
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined;
