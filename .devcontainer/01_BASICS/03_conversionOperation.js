2161e78a4a235d08d7b33110053cfb983b7ee5b2
arpan-bhati-developer <147476575+arpan-bhati-developer@users.noreply.github.com>
2024-09-14 10:59:39 +0000

    Conversion comparioson

--- .devcontainer/01_BASICS/03_conversionOperation.js
+++ .devcontainer/01_BASICS/03_conversionOperation.js
@@ -49,30 +49,96 @@ let valueInNumber5 = Number(score5);
 let isLoggedIn = 1;
 
 let booleanLoggedIn = Boolean(isLoggedIn);
-console.log(booleanLoggedIn);
+// console.log(booleanLoggedIn);
 // In this case value of booleanLoggedIn will be true.
 
 let isLoggedIn1 = 0;
 
 let booleanLoggedIn1 = Boolean(isLoggedIn1);
-console.log(booleanLoggedIn1);
+// console.log(booleanLoggedIn1);
 // In this case value of booleanLoggedIn1 will be false.
 
 let isLoggedIn2 = "Arpan";
 
 let booleanLoggedIn2 = Boolean(isLoggedIn2);
-console.log(booleanLoggedIn2);
+// console.log(booleanLoggedIn2);
 // In this case value of booleanLoggedIn2 will be true.
 
 let isLoggedIn3 = " ";
 
 let booleanLoggedIn3 = Boolean(isLoggedIn3);
-console.log(booleanLoggedIn3);
+// console.log(booleanLoggedIn3);
 // In this case value of booleanLoggedIn3 will be false.
 
 let number = 33;
 
 let numberToString = String(number);
-console.log(numberToString);
-console.log(typeof(numberToString));
+// console.log(numberToString);
+// console.log(typeof(numberToString));
 //In this number will be changed into string and value will be 33 and type will be string.
+
+// ****************************Operations*********************************************
+
+let num = 3;
+let value = -num;
+// console.log(value);
+
+let str1 = "hello";
+let str2 = " Arpan";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log ("1" + 2);// output will be 12.
+// console.log(1 + "2");// output will be 12.
+// console.log("1" + 2 + 2);// output will be 122 as 1 is string so compiler wil consider all other elements as string.
+// console.log(1 + 2 + "2");// output will be 32 as first both the elements will be added and then 2 will be added with 3 as string.
+// console.log(1 + "2" + 2);//output will be 122 
+
+let test1 = 1;
+let test2 = 2;
+let test4 = "2";
+let test3 = test1 + test2 + test4;
+// console.log(test3);
+// console.log(typeof(test3));
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2 + 2;
+
+//Prefix
+let x = 3;
+let y = ++x;
+// console.log([x],[y]);
+// In above line of code y = ++x, the value of x will first increased by 1 then stored in variable y.
+
+// Postfix
+let a = 3;
+let b = a++;
+// console.log([a],[b]);
+// In above case b = a++, value of a will be stored in b first then incrased by 1.
+
+// Comparison 
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// In above case the equality check (==) workd differently as of the comparisons(<,>,>=,<=,=), these comparisons considers null as 0, but not the euality check.
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// In case of undefined all the outputs will be false.
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// Here above comparison operators will convert string "2" and "02"  in number and compare with 1 that's why the output is true.
+
+// Diference between == and === 
+console.log("2" == 2);
+
+// Above case will give us true beacuse the equality check == checks for only the values but not their tyupes, if the values are equal then it gives true.
+
+console.log("2" === 2);
+
+//Above case will give us false becuse equality check === cheks for values as well as their their types if thier types are different then it will give false even of the values are equal.
