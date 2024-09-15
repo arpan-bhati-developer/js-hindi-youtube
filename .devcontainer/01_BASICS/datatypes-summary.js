09403f34c8bd1735af8208d497ce64d7a182ab78
arpan-bhati-developer <147476575+arpan-bhati-developer@users.noreply.github.com>
2024-09-15 12:31:28 +0000

    Stack and Heap

--- .devcontainer/01_BASICS/datatypes-summary.js
+++ .devcontainer/01_BASICS/datatypes-summary.js
@@ -14,7 +14,7 @@ const anotherId = Symbol('123');
 // Both the symbols have the same value but if we compare them then the result will be false.
 // Datatypr of the above variables will be symbol.
 
-console.log(id == anotherId);
+// console.log(id == anotherId);
 
 const bigNumber = 3456789345678889677885545;
 
@@ -41,7 +41,35 @@ const myFunction = function(){
 }
 
 
-console.log(typeof (myObj));
+// console.log(typeof (myObj));
 // datatype of a function is function but vebally it is called as object function.
 
 // datatype of both array and object is object.
+
+//++++++++++++++++++++++++++++++++STACK AND HEAP MEMORY IN JS+++++++++++++++++++++++++++++
+
+// Stack memory is used in Primitive datatypes, Heap is used in Non-Primitive Datatypes.
+// Stack -------
+let myChannel = "hiteshchaudharydotcom";
+let anotherChannel = myChannel;
+anotherChannel = "chai or code";
+
+// console.log(myChannel);
+// console.log(anotherChannel);
+
+// here the value is not changed as primitive datatype uses stavk memory whoch just uses the copy of one variable to another variable.
+
+//Heap ----------------
+let userOne = {
+    email : "arpan@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "arpan@infozech.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// Here both the email are uptaed and became "arpan@infozech.com" as Non-Primitive data types uses Heap memory where the we gave the actual reference not only  the value.
